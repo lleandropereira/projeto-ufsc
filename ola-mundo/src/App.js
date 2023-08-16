f0e5d738364f1c019759dfa34eb3fa4bea86847b
Leandro Pereira <leandrop2104@gmail.com>
2023-08-16 11:55:08 -0300

    Olá mundo e React básico

---
+++ ola-mundo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá mundo</h1>
+      <p>Este é meu primeiro programa em React</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basico/src/components/Component01.js
@@ -0,0 +1,10 @@
+const Component01 = () => {
+  return (
+    <div>    
+       <h2 className="Titulo do meu Componente"> Meu primeiro componente React </h2>
+       {console.log("Mensagem de teste")}
+    </div>
+  )
+}
+
+export default Component01
